5ff851c76e22e310aafc742b84f849da57caa445
nidqija <ajiqdin646@gmail.com>
2025-03-31 01:53:40 +0800

    added new note

--- my-react-app/src/components/menu.jsx
+++ my-react-app/src/components/menu.jsx
@@ -1,11 +1,30 @@
+import { Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import "../App.css";
+import { FaFileCirclePlus } from "react-icons/fa6";
+import Button from 'react-bootstrap/Button';
+
+
 
 
 function MenuPage(){
     return(
-        <Card>
-        <Card.Body className='text-dark'>This is some text within a card body.</Card.Body>
-      </Card>
+      <div className='body'>
+      <div className='headerBackground p-3'>
+        <Container>
+        <h5 className="p-2" style={{fontFamily : "League Spartan" , color : 'white'}}>New Notes</h5>
+      
+      <div className="d-grid gap-2">
+      <Button id='ChoicesMenu'>
+        Add new <FaFileCirclePlus/>
+      </Button>
+   
+    </div>
+
+   
+      </Container>
+      </div>
+      </div>
     )
 }
 
--- my-react-app/src/pages/newNotes.jsx
+++ my-react-app/src/pages/newNotes.jsx
@@ -1,10 +1,12 @@
 import MenuPage from "../components/menu";
 import SideBar from "../components/navbar";
+import NoteTitle from "../components/NoteTitle";
 
 
 function NewNotes(){
     return (
         <>
+        <NoteTitle/>
        <MenuPage/>
        <SideBar/>
        </>
