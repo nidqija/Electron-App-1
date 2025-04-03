16c03876cae8ff35015319308bb6ca25827e5c1a
nidqija <ajiqdin646@gmail.com>
2025-04-03 18:31:18 +0800

    resize the textarea for note taking section

--- my-react-app/src/components/WriteInput.jsx
+++ my-react-app/src/components/WriteInput.jsx
@@ -10,13 +10,13 @@ function WriteNotes() {
     <Form >
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className='Font text-white ' >Title</Form.Label>
-        <Form.Control style={{backgroundColor : " rgb(24, 22, 26)" , color : "white"}} className='text-white' placeholder="Enter title" />
+        <Form.Control style={{backgroundColor : " rgb(24, 22, 26)" , color : "white"}} className='text-white'  />
      
       </Form.Group>
 
     
-      <Form.Label className='text-white'   >Example textarea</Form.Label>
-        <Form.Control as="textarea" style={{backgroundColor : " rgb(24, 22, 26)"}} className='Font mb-5 text-white' />
+      <Form.Label className='text-white'>Description</Form.Label>
+        <Form.Control as="textarea"  rows={13} style={{backgroundColor : " rgb(24, 22, 26)"}} className='Font mb-4 text-white' />
       <Button variant="primary" type="submit">
         Save
       </Button>
