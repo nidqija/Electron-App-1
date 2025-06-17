3985f963f26dacc429daaef81966263d3466ef3d
nidqija <ajiqdin646@gmail.com>
2025-06-17 08:02:44 +0800

    added css injection in intro page

--- my-react-app/src/pages/IntroPage.jsx
+++ my-react-app/src/pages/IntroPage.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { GiPenguin } from "react-icons/gi";
+import { Container } from "react-bootstrap";
 import "../App.css";
 
 
@@ -15,10 +17,12 @@ function Intropage() {
   }, [navigate]);
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1 className="text-white">NoteMate!</h1>
-      <p className="text-white">Welcome to our note page!</p>
+    <Container className="d-flex justify-content-center align-items-center" style={{ textAlign: 'center',height: '100vh' }} >
+    <div>
+      <h1 className="text-white"><GiPenguin /> NoteMate!</h1>
+      <p className="text-white">A note app. But better!</p>
     </div>
+    </Container>
   );
 }
 
