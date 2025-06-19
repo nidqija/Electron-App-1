86e32895c7df36bb974ef061fa857fe2cd6ed398
nidqija <ajiqdin646@gmail.com>
2025-06-20 07:54:05 +0800

    added animation in the intro page

--- my-react-app/src/assets/loading.jsx
+++ my-react-app/src/assets/loading.jsx
@@ -2,7 +2,10 @@ import { Container } from 'react-bootstrap';
 import Spinner from 'react-bootstrap/Spinner';
 
 
+
 function Loading(){
+
+   
    return(
       <Container className='text-center p-5' >
       <Spinner animation="border" role="status" variant='light'>
--- my-react-app/src/pages/IntroPage.jsx
+++ my-react-app/src/pages/IntroPage.jsx
@@ -1,13 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { GiPenguin } from "react-icons/gi";
 import { Container } from "react-bootstrap";
+import {useTransition , animated} from '@react-spring/web';
 import "../App.css";
 
 
 
 function Intropage() {
   const navigate = useNavigate();
+  const [showTitle , setShowTitle] = useState(false);
+
+  useEffect(()=>{
+
+    setShowTitle(true);
+
+
+    const timer2 = setTimeout(()=>{
+      setShowTitle(false);
+
+    },2000);
+
+    return () => clearTimeout(timer2);
+  },[]);
+
+
+  const transition = useTransition(showTitle,{
+    from:{opacity:0,transform: 'translateY(20px)'},
+    enter:{opacity:1,transform: 'translateY(0px)' },
+    leave:{opacity:0,transform: 'translateY(-20px)'},
+    config:{duration:1000},
+  })
+
+
+
 
    useEffect(() => {
     const timer = setTimeout(() => {
@@ -18,12 +44,21 @@ function Intropage() {
   }, [navigate]);
 
   return (
+    <>
     <Container className="d-flex justify-content-center align-items-center" style={{ textAlign: 'center',height: '100vh' }} >
-    <div>
-      <h1 className=" text-white" id="example-fade-text"><GiPenguin /> NoteMate!</h1>
-      <p className="text-white" id="example-fade-text ">A note app. But better!</p>
+    <div >
+      {transition((style,item)=>
+      item?(
+     <div>
+      <animated.h1 className=" text-white" style={style} ><GiPenguin /> NoteMate!</animated.h1>
+      <animated.p className="text-white" style={style}>A note app. But better!</animated.p>
+     </div>
+      ) :null
+      )}
+   
     </div>
     </Container>
+    </>
   );
 }
 
